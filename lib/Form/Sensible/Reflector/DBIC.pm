961bee357ee832be256554bf2ecf215868efb011
Devin Austin <dhoss@squishface.com>
2010-02-22 17:25:34 -0700

    upped version changelog and such

--- lib/Form/Sensible/Reflector/DBIC.pm
+++ lib/Form/Sensible/Reflector/DBIC.pm
@@ -2,7 +2,7 @@ package Form::Sensible::Reflector::DBIC;
 use Moose;
 use namespace::autoclean;
 extends 'Form::Sensible::Reflector';
-our $VERSION = "0.033";
+our $VERSION = "0.034";
 
 # ABSTRACT: A Form::Sensible::Form::Reflector subclass to reflect off of DBIC schema classes
 
