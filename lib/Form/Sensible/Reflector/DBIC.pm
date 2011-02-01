8ae2c42404479f7f42a747b00224540fe6de7a19
Devin Austin <dhoss@cpan.org>
2011-02-01 01:53:28 -0700

    version bump, changelog updated

--- lib/Form/Sensible/Reflector/DBIC.pm
+++ lib/Form/Sensible/Reflector/DBIC.pm
@@ -2,7 +2,7 @@ package Form::Sensible::Reflector::DBIC;
 use Moose;
 use namespace::autoclean;
 extends 'Form::Sensible::Reflector';
-our $VERSION = "0.0346";
+our $VERSION = "0.0347";
 $VERSION = eval $VERSION;
 
 use Data::Dumper;
