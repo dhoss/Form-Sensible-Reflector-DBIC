145e424969a149180c57fa3b1f45967399084325
Devin Austin <dhoss@cpan.org>
2012-01-03 12:02:34 -0700

    updated role docs

--- lib/Form/Sensible/Reflector/DBIC/Role/FieldClassOptions.pm
+++ lib/Form/Sensible/Reflector/DBIC/Role/FieldClassOptions.pm
@@ -2,6 +2,58 @@ package Form::Sensible::Reflector::DBIC::Role::FieldClassOptions;
 use Moose::Role;
 use namespace::autoclean;
 
+=head1 NAME
+
+Form::Sensible::Reflector::DBIC::Role::FieldClassOptions - Sane defaults for field classes.
+
+=cut
+
+
+=head1 $self->field_class_options
+
+Hashref of default field class options.
+
+Current incarnation: 
+
+    'Number' => {
+        'validation' => {
+          'integer_only' => 'integer_only',
+          'upper_bound'  => 'upper_bound',
+          'lower_bound'  => 'lower_bound',
+          'step'         => 'step',
+        },
+      },
+      'Toggle' => {
+        'render_hints' => {
+          'on_value'  => 'on_value',
+          'on_label'  => 'on_label',
+          'off_value' => 'off_value',
+          'off_label' => 'off_label',
+        },
+      },
+      'Text' => {
+        'validation' => {
+          'size'            => 'maximum_length',
+          'minimum_length'  => 'minimum_length',
+          'maximum_length'  => 'maximum_length',
+          'should_truncate' => 'should_truncate',
+        },
+      },
+      'LongText' => {
+        'validation' => {
+          'size'            => 'maximum_length',
+          'minimum_length'  => 'minimum_length',
+          'maximum_length'  => 'maximum_length',
+          'should_truncate' => 'should_truncate',
+        },
+      },
+      'FileSelector' => { 'validation' => { 'size' => 'maximum_size', }, },
+      'Select' =>
+        { 'validation' => { 'options_delegate' => 'options_delegate', }, },
+
+
+=cut
+
 has 'field_class_options' => (
   is         => 'rw',
   isa        => 'HashRef',
--- lib/Form/Sensible/Reflector/DBIC/Role/FieldTypeMap.pm
+++ lib/Form/Sensible/Reflector/DBIC/Role/FieldTypeMap.pm
@@ -2,6 +2,61 @@ package Form::Sensible::Reflector::DBIC::Role::FieldTypeMap;
 use Moose::Role;
 use namespace::autoclean;
 
+=head1 NAME
+
+Form::Sensible::Reflector::DBIC::Role::FieldTypeMap - Map generic SQL data types to L<Form::Sensible> field types.
+
+=cut
+
+=head1 $self->field_type_map
+
+Define a hashref of field->types.
+
+Current incarnation is as follows:
+
+      varchar  => { defaults => { field_class => 'Text', }, },
+      text     => { defaults => { field_class => 'LongText', }, },
+      blob     => { defaults => { field_class => 'FileSelector' }, },
+      datetime => { defaults => { field_class => 'Text', }, },
+      enum     => { defaults => { field_class => 'Select', }, },
+      int      => {
+        defaults => {
+          field_class  => 'Number',
+          integer_only => 1,
+        },
+      },
+      integer => {
+        defaults => {
+          field_class  => 'Number',
+          integer_only => 1,
+        },
+      },
+      bigint => {
+        defaults => {
+          field_class  => 'Number',
+          integer_only => 1,
+        },
+      },
+      bool => {
+        defaults => {
+          field_class => 'Toggle',
+          on_value    => 1,
+          on_label    => 'yes',
+          off_value   => 0,
+          off_label   => 'no'
+        },
+      },
+      decimal => {
+        defaults => {
+          field_class  => 'Number',
+          integer_only => 0,
+        },
+      },
+    };
+
+
+=cut
+
 has field_type_map => (
   is         => 'rw',
   isa        => 'HashRef',
@@ -51,5 +106,14 @@ sub _build_field_type_map {
       },
     };
 };
+
+
+=head1 AUTHOR
+
+Devin Austin  L<mailto:devin.austin@ionzero.com>
+
+Andre Walker  L<http://andrewalker.net>
+
+=cut
 
 1;
